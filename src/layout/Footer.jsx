63ef7028cb23ac39a04e1bed05409fab46408f22
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-07-05 13:50:25 -0400

    Set bg color of footer

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,6 +1,6 @@
 export function Footer() {
    return (
-      <footer className="flex p-4 justify-center items-center mt-auto">
+      <footer className="flex p-4 justify-center items-center mt-auto bg-inherit">
          <p>Copyright © 2024 Mahmoud T</p>
       </footer>
    );
