9a2cfa824c687472b83ba9c06e519ad78557610a
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-07-04 21:14:08 -0400

    Create layout including header footer and main container

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,7 @@
+export function Footer() {
+   return (
+      <footer className="flex p-4 justify-center items-center mt-auto">
+         <p>Copyright © 2024 Mahmoud T</p>
+      </footer>
+   );
+}
---
+++ src/layout/Layout.jsx
@@ -0,0 +1,12 @@
+import { Footer } from "./Footer";
+import { Header } from "./Header";
+
+export function Layout() {
+   return (
+      <>
+         <Header />
+         <main></main>
+         <Footer />
+      </>
+   );
+}
