185ee7600a3adb7a1ec9d4d72dfb2178110bce2d
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-07-04 21:16:05 -0400

    Style header

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,7 +1,7 @@
 export function Header() {
    return (
-      <header>
-         <h1>Tag Quest</h1>
+      <header className="p-6">
+         <h1 className="text-3xl font-bold">Tag Quest</h1>
       </header>
    );
 }
