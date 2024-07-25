cf3ffd0dd1ac705e7dbe9f392ef71de3e679e54a
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-07-25 19:10:02 -0400

    Remove header element since its redundant and doesn't look good

--- src/layout/Header.jsx
+++
@@ -1,7 +0,0 @@
-export function Header() {
-   return (
-      <header className="p-6">
-         <h1 className="text-3xl font-bold">Tag Quest</h1>
-      </header>
-   );
-}
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,11 +1,9 @@
 import { Outlet } from "react-router-dom";
 import { Footer } from "./Footer";
-import { Header } from "./Header";
 
 export function Layout() {
    return (
       <>
-         <Header />
          <main>
             <Outlet />
          </main>
