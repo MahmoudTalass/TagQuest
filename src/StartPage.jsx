4a9bf61e92a526c724ae229f7d81860400a0f5d4
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-07-25 19:41:54 -0400

    Improve aesthetic of starting page

--- src/StartPage.jsx
+++ src/StartPage.jsx
@@ -26,26 +26,34 @@ export function StartPage() {
 
    return (
       <section className="mt-10 flex items-center justify-center">
-         <div className="border w-full max-w-[700px] flex flex-col items-center p-8 gap-4">
-            <h2>Welcome!</h2>
-            <p>Your task is to find the following characters:</p>
-            <div className="flex gap-8 items-end">
-               {characters.map((character) => {
-                  return (
-                     <div key={character._id} className="flex flex-col items-center">
-                        <img
-                           src={character.image}
-                           alt={`image of ${character.name}`}
-                           className="w-40 hover:scale-105"
-                        />
-                        <p>{character.name}</p>
-                     </div>
-                  );
-               })}
+         <div className="bg-color1 rounded-lg shadow-lg">
+            <div className="w-full max-w-[700px] flex flex-col items-center p-8 gap-12">
+               <div className="flex items-center w-full">
+                  <h1 className="text-3xl font-bold grow ">Wecome to Tag Quest!</h1>
+                  <p>Leaderboard</p>
+               </div>
+
+               <div className="flex flex-col gap-8">
+                  <p className="text-lg">Your task is to find the following characters:</p>
+                  <div className="flex gap-16 items-end">
+                     {characters.map((character) => {
+                        return (
+                           <div key={character._id} className="flex flex-col items-center">
+                              <img
+                                 src={character.image}
+                                 alt={`image of ${character.name}`}
+                                 className="w-40 hover:scale-105"
+                              />
+                              <p>{character.name}</p>
+                           </div>
+                        );
+                     })}
+                  </div>
+               </div>
+               <Link to={"/gameplay"}>
+                  <button className="mt-6 bg-green-500 rounded-lg px-8 py-3 text-md">Start</button>
+               </Link>
             </div>
-            <Link to={"/gameplay"}>
-               <button className="mt-4 border rounded-md px-6 py-2 text-lg">Start</button>
-            </Link>
          </div>
       </section>
    );
